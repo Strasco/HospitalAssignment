37937a996615e6a05b50c2e599dc6aea91cc581a
Vlad Zaharia <vlad.zaharia95@gmail.com>
2020-04-13 14:54:53 +0200

    Initial Commit

---
+++ Model/Medic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EssensysHospitalWPF.Model
+{
+    class Medic
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ForName { get; set; }
+        public string CNP { get; set; }
+        public DateTime HiredDate { get; set; }
+        public string UniversityGraduated { get; set; }
+        public int ResidencyYearDuration { get; set; } //Durata rezidentiatului
+            
+    }
+}
